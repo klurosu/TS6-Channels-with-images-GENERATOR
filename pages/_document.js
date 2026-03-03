bcacea7fec66951ff53ee8893a7f1014c5bb3a1b
klurosu <klurosu@gmail.com>
2026-03-03 16:57:22 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -33,30 +33,15 @@ export default class MyDocument extends Document {
             <Html>
                 <Head>
                     <meta property={'description'} content={`Tool for generating background cover images for channels in
-                     new TeamSpeak 5 beta client. Upload any image and it will be split into multiple images that you 
+                     new TeamSpeak 6 client. Upload any image and it will be split into multiple images that you 
                      can simply use as banners on your channels.`}/>
 
-                    <meta property="og:title" content="TeamSpeak 5 Channel Image Generator"/>
-                    <meta property="og:url" content="https://pitkes22.github.io/ts5-channel-image-generator/"/>
+                    <meta property="og:title" content="TeamSpeak 6 Channel Image Generator"/>
                     <meta property="og:description" content="Tool for generating background cover images for channels in
-                    new TeamSpeak 5 beta client. Upload any image and it will be split into multiple images that you "/>
+                    new TeamSpeak 6 client. Upload any image and it will be split into multiple images that you "/>
                     <meta property="og:type" content="website"/>
                     <meta property="og:image" content="https://i.imgur.com/5YSmahN.png"/>
-
-                    <script async
-                            src="https://www.googletagmanager.com/gtag/js?id=G-ZKRME2HRL0"></script>
-                    <script dangerouslySetInnerHTML={{
-                        __html: `
-                    <!-- Global site tag (gtag.js) - Google Analytics -->
-                          window.dataLayer = window.dataLayer || [];
-                          function gtag(){dataLayer.push(arguments);}
-                          gtag('js', new Date());
-                        
-                          gtag('config', 'G-ZKRME2HRL0');
-                    `
-                    }}/>
                 </Head>
-
                 <body className={'bp3-dark'}>
                 <Main/>
                 <NextScript/>
